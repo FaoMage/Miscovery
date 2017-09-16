1219c98f14b99cd19f1175754fadd7597fed6e4b
Unknown <fernando.orgeira@gmail.com>
2017-09-16 19:35:03 -0300

    Funcionamiento Lista
    - Adapter creado
    - Funcionamiento de la lista en casi su totalidad

--- app/src/main/java/com/dh/agus/digitalhousemusic/MainActivityFragment.java
+++ app/src/main/java/com/dh/agus/digitalhousemusic/MainActivityFragment.java
@@ -3,9 +3,15 @@ package com.dh.agus.digitalhousemusic;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,17 +19,63 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
-
-    public MainActivityFragment() {
-        // Required empty public constructor
-    }
-
+    private List<String> songsList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_activity, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_activity, container, false);
+
+        loadSongs();
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView_MainActivity);
+        RecyclerViewAdapter.RecyclerViewInterface listener = new RecyclerViewAdapter.RecyclerViewInterface() {
+            @Override
+            public void favoriteOnClick() {
+                Toast.makeText(getContext(), "Funcion: Agregar a favoritos", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void menuOnClick() {
+                Toast.makeText(getContext(), "Funcion: Abrir menu de cancion", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void songOnClick() {
+                Toast.makeText(getContext(), "Funcion: Ir a la reproduccion", Toast.LENGTH_SHORT).show();
+            }
+        };
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(getContext(),songsList,listener);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+
+    // Metodo que carga una base de datos para probar la lista
+    private void loadSongs () {
+        songsList.add("Musica1");
+        songsList.add("Musica2");
+        songsList.add("Musica3");
+        songsList.add("Musica4");
+        songsList.add("Musica5");
+        songsList.add("Musica6");
+        songsList.add("Musica7");
+        songsList.add("Musica8");
+        songsList.add("Musica9");
+        songsList.add("Musica10");
+        songsList.add("Musica11");
+        songsList.add("Musica12");
+        songsList.add("Musica13");
+        songsList.add("Musica14");
+        songsList.add("Musica15");
+        songsList.add("Musica16");
+        songsList.add("Musica17");
+        songsList.add("Musica18");
+        songsList.add("Musica19");
+        songsList.add("Musica20");
     }
 
 }
---
+++ app/src/main/java/com/dh/agus/digitalhousemusic/RecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package com.dh.agus.digitalhousemusic;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ZetaxMage on 16/09/2017.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context context;
+    private List<String> songsList;
+    private RecyclerViewInterface listener;
+
+    public RecyclerViewAdapter (Context context, List<String> songsList, RecyclerViewInterface listener) {
+        this.context = context;
+        this.songsList = songsList;
+        this.listener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.structure_recycler_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String songName = songsList.get(position);
+        holder.loadView(songName);
+
+        // onclicks codeados de forma rara para mostrar que van a hacer
+        holder.itemView.findViewById(R.id.imageViewFavorite).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.favoriteOnClick();
+            }
+        });
+        holder.itemView.findViewById(R.id.imageViewMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.menuOnClick();
+            }
+        });
+        holder.textViewNameSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.songOnClick();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return songsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView textViewNameSong;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textViewNameSong = itemView.findViewById(R.id.textView_SongName);
+        }
+
+        public void loadView (String songName) {
+            textViewNameSong.setText(songName);
+        }
+    }
+
+    public interface RecyclerViewInterface {
+        void favoriteOnClick ();
+        void menuOnClick ();
+        void songOnClick();
+    }
+}
